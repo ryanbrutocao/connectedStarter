424db43c69c978ad25a4450bb3ed4c7cbe5a641d
Ryan Brutocao <ryanbrutocao@outlook.com>
2020-02-09 14:04:06 -0700

    fixed keys

--- server/config/keys.js
+++ server/config/keys.js
@@ -2,5 +2,5 @@
 // pw:"iz8YAjowlEMdhHW8"
 module.exports = {
   mongoURI:
-    "mongodb+srv://ryan:iz8YAjowlEMdhHW8@cluster0-8sliz.mongodb.net/test?retryWrites=true&w=majority"
+    // "mongodb+srv://ryan:iz8YAjowlEMdhHW8@cluster0-8sliz.mongodb.net/test?retryWrites=true&w=majority"
 };
