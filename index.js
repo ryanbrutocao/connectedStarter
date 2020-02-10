5cd10d83567f9c5553fa66bc0428118dc3ea8b15
Ryan Brutocao <ryanbrutocao@outlook.com>
2020-02-09 21:58:57 -0700

    working code, connected to tasker

--- index.js
+++ index.js
@@ -1,58 +1,30 @@
-// // /index.js
-// const express = require('express');
-// const mongoose = require('mongoose');
-// const bodyParser = require('body-parser');
-// const keys = require("./server/config/keys")
 
-// // IMPORT MODELS
-// require('./models/Tasks');
-
-// const app = express();
-
-// mongoose.Promise = global.Promise;
-// mongoose.connect(keys.mongoURI)
-// // mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost:27017/node-react-starter`);
-
-// app.use(bodyParser.json());
-
-// //IMPORT ROUTES
-// require('./routes/taskRoutes')(app);
-
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static('client/build'));
-
-//   const path = require('path');
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-//   })
-
-// }
-
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => {
-//   console.log(`app running on port ${PORT}`)
-// });
-// /index.js
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const keys = require("./server/config/keys")
+const keys = require("./server/config/keys.js");
 
+const bodyParser = require('body-parser');
 
 // IMPORT MODELS
-require('./models/Product');
+require('./models/Task');
 
 const app = express();
 
-mongoose.Promise = global.Promise;
-mongoose.connect(keys.mongoURI)
-mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost:27017/node-react-starter`);
+// mongoose.Promise = global.Promise;
+// mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost:27017/node-react-starter`);
+mongoose.connect(keys.mongoURI);
+
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //IMPORT ROUTES
-require('./routes/productRoutes')(app);
+require('./routes/taskRoutes')(app);
+//generates a new application that represents a running express app
+const routes = require("./routes/taskRoutes");
+app.use(routes);
 
+// This will redirect all the requests to our frontend application, unless we specify any route before this code.
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 
@@ -62,7 +34,7 @@ if (process.env.NODE_ENV === 'production') {
   })
 
 }
-
+//  
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`app running on port ${PORT}`)
--- models/Task.js
+++ models/Task.js
@@ -1,14 +1,17 @@
-
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-// const taskSchema = new Schema({
-const productSchema = new Schema({
-  name: String,
-  description: String
+const taskSchema = new Schema({
+
+  title: String,
+  category: String,
+  description: String,
+  due_by: String,
+  created: String,
+  status: String
 })
 
-mongoose.model('products', productSchema);
+mongoose.model('tasks', taskSchema);
 
 // name: String,
 // title: String,
